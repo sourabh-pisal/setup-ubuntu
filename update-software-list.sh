f0af871e7bdf50aa973f203ea6f05b45d93f2905
Sourabh Pisal <pisal.sourabh@gmail.com>
2024-11-03 20:46:18 +0000

    Update package list

--- update-software-list.sh
+++ update-software-list.sh
@@ -2,4 +2,4 @@
 set -e 
 
 # Get the latest package list from pacman ignore archtiecture specific packages
-pacman -Qqen | grep -v amd-ucode | grep -v base | grep -v linux | grep -v linux-firmware > package-list.txt
+pacman -Qqen | grep -v amdvlk | grep -v amd-ucode | grep -v base | grep -v linux | grep -v linux-firmware > package-list.txt
